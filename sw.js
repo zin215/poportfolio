85a71b2b000063b8c15e50ec5f33151226137e18
zin215 <lkj1112@gmail.com>
2026-06-13 14:49:50 +0900

    첫 배포

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('v1').then((cache) => {
+    return cache.addAll(['./index.html']);
+  }));
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
